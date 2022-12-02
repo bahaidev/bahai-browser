eb2a0f8da92558febb8fdc4c7853df62128f69ff
Brett Zamir <brettz9@yahoo.com>
2022-12-02 15:15:05 -0700

    test: coverage

--- cypress/e2e/workDisplay.cy.js
+++ cypress/e2e/workDisplay.cy.js
@@ -8,47 +8,127 @@ describe('Work display page', () => {
       return registration.unregister();
     }));
   });
-  beforeEach(() => {
-    cy.visit('http://localhost:8000/index-instrumented.html#lang=en-US&work=aqdas');
-  });
 
-  it('passes accessibility', function () {
-    // See https://github.com/component-driven/cypress-axe
-    cy.injectAxe();
-    cy.configureAxe({
-      rules: [
-        // A bit ugly if visually adding, but could add
-        {
-          id: 'page-has-heading-one',
-          enabled: false
-        }
-      ]
+  describe('Regular pages', function () {
+    beforeEach(() => {
+      cy.visit('http://localhost:8000/index-instrumented.html#lang=en-US&work=aqdas');
     });
-    cy.get('div[role=main]'); // Wait until available before checking a11y
-    cy.checkA11y();
+    it('passes accessibility', function () {
+      // See https://github.com/component-driven/cypress-axe
+      cy.injectAxe();
+      cy.configureAxe({
+        rules: [
+          // A bit ugly if visually adding, but could add
+          {
+            id: 'page-has-heading-one',
+            enabled: false
+          }
+        ]
+      });
+      cy.get('div[role=main]'); // Wait until available before checking a11y
+      cy.checkA11y();
+    });
+
+    it('"Check all" enables all fields', function () {
+      // Indexes
+      cy.get('input#checked1').should('not.be.checked');
+      // Tooltips (English)
+      cy.get('input#checked2').should('be.checked');
+      cy.get('input[type="button"][value="Check all"]').click();
+      cy.get('input#checked1').should('be.checked');
+      cy.get('input#checked2').should('be.checked');
+    });
+
+    it('"Uncheck all" disables all fields', function () {
+      // Indexes
+      cy.get('input#checked1').should('not.be.checked');
+      // Tooltips (English)
+      cy.get('input#checked2').should('be.checked');
+      cy.get('input[type="button"][value="Uncheck all"]').click();
+      cy.get('input#checked1').should('not.be.checked');
+      cy.get('input#checked2').should('not.be.checked');
+    });
   });
 
-  it('"Check all" enables all fields', function () {
-    // Indexes
-    cy.get('input#checked1').should('not.be.checked');
-    // Tooltips (English)
-    cy.get('input#checked2').should('be.checked');
-    cy.get('input[type="button"][value="Check all"]').click();
+  describe('Multiple browse fields', function () {
+    it('builds multiple browse fields', function () {
+      cy.visit('http://localhost:8000/index-instrumented.html#lang=en-US&work=quran');
+      cy.get('#quran-start1-1');
+      cy.get('#quran-start1-2');
+      cy.get('#quran-start2-1');
+      cy.get('#quran-start2-2');
+    });
+
+    it('has start field trigger default for end field', function () {
+      cy.visit('http://localhost:8000/index-instrumented.html#lang=en-US&work=quran');
+      cy.get('#quran-end1-1').should('be.empty');
+      cy.get('#quran-start1-1').type('The Cow (2)').blur();
+      cy.get('#quran-end1-1').should('have.value', 'The Cow (2)');
+    });
+  });
+
+  it('allows presetting of form control values', function () {
+    cy.visit('http://localhost:8000/index-instrumented.html#lang=en-US&work=aqdas&aqdas-start1-1=1&aqdas-end1-1=20&aqdas-anchor1-1=10&anchorfield1=Paragraph&field1=Indexes+%28English%29&interlin1=&css1=&field2=Tooltips+%28English%29&interlin2=&css2=&field3=Locally-stored+notes&interlin3=&css3=&field4=Speech+Arabic&interlin4=&css4=&field5=Paragraph&interlin5=&css5=&field6=English&interlin6=&css6=&field7=German&interlin7=&css7=&field8=Arabic&interlin8=&css8=&field9=Editable+wiki+page&interlin9=&css9=&field10=Links+to+wiki&interlin10=&css10=&field11=Links+to+user+wiki&interlin11=&css11=&colorName=Blue&color=%23&bgcolorName=Yellow&bgcolor=%23&fontSeq=Palatino%2C+serif&fontstyle=oblique&fontvariant=small-caps&fontweight=300&fontsize=14px&fontstretch=expanded&letterspacing=.2em&lineheight=1.1&header=y&footer=y&caption=y&border=1&interlintitle=1&interlintitle_css=&pagecss=&outputmode=table&checked1=Yes&checked2=Yes&checked3=Yes&checked4=Yes&checked5=Yes&checked6=Yes&checked7=Yes&checked8=Yes&checked9=No&checked10=Yes&checked11=Yes&headerfooterfixed=Yes');
     cy.get('input#checked1').should('be.checked');
     cy.get('input#checked2').should('be.checked');
-  });
-
-  it('"Uncheck all" disables all fields', function () {
-    // Indexes
-    cy.get('input#checked1').should('not.be.checked');
-    // Tooltips (English)
-    cy.get('input#checked2').should('be.checked');
-    cy.get('input[type="button"][value="Uncheck all"]').click();
-    cy.get('input#checked1').should('not.be.checked');
-    cy.get('input#checked2').should('not.be.checked');
+    cy.get('input#checked9').should('not.be.checked');
+    cy.get('#aqdas-start1-1').should('have.value', '1');
+    cy.get('#aqdas-end1-1').should('have.value', '20');
+    cy.get('#aqdas-anchor1-1').should('have.value', '10');
+    cy.get(
+      'select[name="anchorfield1"]'
+    ).find('option').contains('Paragraph').should('be.selected');
+    cy.get(
+      'select[name="colorName"]'
+    ).find('option[value="Blue"]').should('be.selected');
+    cy.get(
+      'select[name="bgcolorName"]'
+    ).find('option[value="Yellow"]').should('be.selected');
+    cy.get(
+      'select[name="fontSeq"]'
+    ).find('option[value="Palatino, serif"]').should('be.selected');
+    cy.get(
+      'select[name="fontstyle"]'
+    ).find('option[value="oblique"]').should('be.selected');
+    cy.get(
+      'input[name="fontvariant"][value="small-caps"]'
+    ).should('be.checked');
+    cy.get(
+      'input[name="fontweight"]'
+    ).should('have.value', '300');
+    cy.get(
+      'input[name="fontsize"]'
+    ).should('have.value', '14px');
+    cy.get(
+      'select[name="fontstretch"]'
+    ).find('option[value="expanded"]').should('be.selected');
+    cy.get(
+      'input[name="letterspacing"]'
+    ).should('have.value', '.2em');
+    cy.get(
+      'input[name="lineheight"]'
+    ).should('have.value', '1.1');
+    cy.get(
+      'input[name="header"][value="y"]'
+    ).should('be.checked');
+    cy.get(
+      'input[name="footer"][value="y"]'
+    ).should('be.checked');
+    cy.get(
+      'input[name="headerfooterfixed"][value="Yes"]'
+    ).should('be.checked');
+    cy.get(
+      'input[name="caption"][value="y"]'
+    ).should('be.checked');
+    cy.get(
+      'input[name="border"][value="1"]'
+    ).should('be.checked');
   });
 
   describe('Preferences', function () {
+    beforeEach(() => {
+      cy.visit('http://localhost:8000/index-instrumented.html#lang=en-US&work=aqdas');
+    });
     it('hides preferences', function () {
       cy.get('#preferences').should('not.be.visible');
       cy.get('button').contains('Preferences').click();
